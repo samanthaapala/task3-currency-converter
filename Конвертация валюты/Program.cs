3c6df3ce81651e9f878c6e8ae6e5585809f349bc
Samantha Apala <s.apala@yandex.ru>
2021-05-29 16:34:06 +0300

    Добавьте файлы проекта.

---
+++ Конвертация валюты/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертация_валюты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите рублевое значение");
+            decimal decimalRuble = Convert.ToInt32(Console.ReadLine());
+            decimal decimalDollar = decimalRuble * 76;
+            Console.WriteLine($"{decimalRuble} руб. равно {decimalDollar}$");
+            Console.ReadKey();
+
+        }
+
+    }
+}
